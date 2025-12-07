5f391a19c9183e45090a183d7a98b430c8590fdf
PhanVanManh <imseirousmanh@gmail.com>
2025-12-07 18:55:48 +0700

    add Header and AddTasks

--- frontend/src/components/AddTasks.jsx
+++ frontend/src/components/AddTasks.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Card } from "./ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const AddTasks = () => {
-  return <div>AddTasks</div>;
+  return (
+    <Card className="p-6 border-0 bg-gradient-card shadow-custom-lg">
+      <div className="flex flex-col gap-3 sm:flex-row">
+        <Input type="text" placeholder="Bạn muốn làm gì nào ?" />
+        <Button>+ Thêm</Button>
+      </div>
+    </Card>
+  );
 };
 
 export default AddTasks;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,50 @@
+import AddTasks from "@/components/AddTasks";
+import DateTimeFilter from "@/components/DateTimeFilter";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import StatsAndFilters from "@/components/StatsAndFilters";
+import TaskList from "@/components/TaskList";
+import TaskListPagination from "@/components/TaskListPagination";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>HomePage
+    <div className="min-h-screen w-full relative">
+      {/* Aurora Dream Soft Harmony */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background: `
+       radial-gradient(ellipse 80% 60% at 60% 20%, rgba(175, 109, 255, 0.50), transparent 65%),
+        radial-gradient(ellipse 70% 60% at 20% 80%, rgba(255, 100, 180, 0.45), transparent 65%),
+        radial-gradient(ellipse 60% 50% at 60% 65%, rgba(255, 235, 170, 0.43), transparent 62%),
+        radial-gradient(ellipse 65% 40% at 50% 60%, rgba(120, 190, 255, 0.48), transparent 68%),
+        linear-gradient(180deg, #f7eaff 0%, #fde2ea 100%)
+      `,
+        }}
+      />
+      <div className="container pt-8 mx-auto relative z-10">
+        <div className="w-full max-w-2xl mx-auto space-y-6">
+          {/*Header */}
+          <Header />
+
+          {/* Tạo nhiệm vụ */}
+          <AddTasks />
+
+          {/* Thống kê và bộ lọc*/}
+          <StatsAndFilters />
+
+          {/* Danh sách công việc*/}
+          <TaskList />
+
+          <div className="flex flex-col items-center justify-between gap-6 sm-flex-row">
+            <TaskListPagination />
+            <DateTimeFilter />
+          </div>
+
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
